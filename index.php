e1c16c0fc7c4117370a28d6d9ca40072c7876214
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-09-18 01:31:31 -0500

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,33 @@
   </head>
   <body>
     <div class="container">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="post.php">Post</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="post-result.php">Post Result</a>
+        </li> 
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="get.php">Get</a>
+        </li> 
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="get-result.php">Get Result</a>
+        </li>      
+      </ul>
+    </div>
+  </div>
+</nav>
     <h1>Hello, world!</h1>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
