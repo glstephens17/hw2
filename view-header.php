9eafb2b1cf73b581dff3ffef0d4e048805b34951
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-11-06 20:27:34 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -55,7 +55,21 @@ document.getElementById("demo").innerHTML = "My First JavaScript";
 </script>
 
 </body>
+</html>
+        <!DOCTYPE html>
+<html>
+<body>
+
+<h2>My First Web Page</h2>
+<p>My first paragraph.</p>
+
+<script>
+window.alert(5 + 6);
+</script>
+
+</body>
 </html> 
+
 
       </ul>
     </div>
