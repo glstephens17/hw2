45e50e45322a66e98524640abd124b95292d4488
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-11-06 19:59:54 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -24,8 +24,6 @@
 <html>
 <body>
 
-<h2>What Can JavaScript Do?</h2>
-
 <p id="demo">JavaScript can change the style of an HTML element.</p>
 
 <button type="button" onclick="document.getElementById('demo').style.fontSize='35px'">Click Me!</button>
