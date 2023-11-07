9d52431a01b2d0c6f4bf4f319749049c88ca6bf2
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-11-06 21:56:21 -0600

    Update view-footer.php

--- view-footer.php
+++ view-footer.php
@@ -1,38 +1,32 @@
-</div class="container">
-<html>
-<body>
-
-<h2>JavaScript Alert</h2>
-
-<button onclick="myFunction()">Try it</button>
-
-<script>
-function myFunction() {
-  alert("Learning is fun!");
-}
-</script>
-
-</body>
-</html>
-</html>
 <!DOCTYPE html>
 <html>
+<head>
+    <title>JavaScript Example</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</head>
 <body>
+    <div class="container">
+        <h2>JavaScript Alert</h2>
 
-<h2>JavaScript Timing</h2>
+        <button onclick="myFunction()">Try it</button>
 
-<p>Click "Try it". Wait 3 seconds, and the page will alert "Hello".</p>
+        <script>
+            function myFunction() {
+                alert("Learning is fun!");
+            }
+        </script>
 
-<button onclick="setTimeout(myFunction, 3000);">Try it</button>
+        <h2>JavaScript Timing</h2>
 
-<script>
-function myFunction() {
-  alert('Hello');
-}
-</script>
+        <p>Click "Try it". Wait 3 seconds, and the page will alert "Hello".</p>
 
+        <button onclick="setTimeout(myFunction, 3000);">Try it</button>
+
+        <script>
+            function myFunction() {
+                alert('Hello');
+            }
+        </script>
+    </div>
 </body>
 </html>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-  </body>
-</div>
