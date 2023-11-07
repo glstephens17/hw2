a4b90f2bbdc4426669440a487ad8d4c74686d72e
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-11-06 19:57:57 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -20,6 +20,19 @@
           <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <!DOCTYPE html>
+        <!DOCTYPE html>
+<html>
+<body>
+
+<h2>What Can JavaScript Do?</h2>
+
+<p id="demo">JavaScript can change the style of an HTML element.</p>
+
+<button type="button" onclick="document.getElementById('demo').style.fontSize='35px'">Click Me!</button>
+
+</body>
+</html> 
+
 <html>
 <body>
 
