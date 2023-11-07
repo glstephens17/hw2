a053220bba3a9b83e353ab34dafe4c4d0d7970eb
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-11-06 20:18:56 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -37,8 +37,6 @@
 <button type="button"
 onclick="document.getElementById('demo').innerHTML = Date()">
 Click me to display Date and Time.</button>
-  .clr1{
-  background-color: #9B0FDB;}
 
 <p id="demo"></p>
 
