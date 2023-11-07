5343c18c1c14ec0821aec449d16f79efbc5f1dd3
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-11-07 00:52:17 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -1,75 +1,29 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?=$pageTitle?></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  </head>
-  <body>
-    <div class="container">
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">HW5 Home</a>
-        </li>
-        <!DOCTYPE html>
-        <!DOCTYPE html>
-<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title><?= $pageTitle ?></title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
 <body>
-
-<p id="demo">JavaScript can change the style of an HTML element.</p>
-
-<button type="button" onclick="document.getElementById('demo').style.fontSize='25px'">Click to enlarge font!</button>
-
-</body>
-</html> 
-
-<html>
-<body>
-
-<button type="button"
-onclick="document.getElementById('demo').innerHTML = Date()">
-Click me to display Date and Time.</button>
-
-<p id="demo"></p>
-
-</body>
-</html> 
-        <!DOCTYPE html>
-<html>
-<body>
-
-<p id="demo"></p>
-
-<script>
-document.getElementById("demo").innerHTML = "My First JavaScript";
-</script>
-
-</body>
-</html>
-        <!DOCTYPE html>
-<html>
-<body>
-
-<h2>     Practice Makes Better</h2>
-
-<script>
-window.alert(JavaScript Practice);
-</script>
-
-</body>
-</html>
-        <!DOCTYPE html>
-        <!DOCTYPE html>
-      </ul>
-    </div>
+  <div class="container">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/">HW5 Home</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- Your other content goes here -->
   </div>
-</nav>
-      
+</body>
+</html>
