3ee84eb1a9643f2353c581cbe1862677cc6babd5
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-11-06 20:56:11 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -26,7 +26,7 @@
 
 <p id="demo">JavaScript can change the style of an HTML element.</p>
 
-<button type="button" onclick="document.getElementById('demo').style.fontSize='25px'">Click Me!</button>
+<button type="button" onclick="document.getElementById('demo').style.fontSize='25px'">Click to enlarge font!</button>
 
 </body>
 </html> 
