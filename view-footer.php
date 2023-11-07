66dc73c89ceb99ac36f44d0998e7cda8d20a36af
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-11-06 21:15:55 -0600

    Update view-footer.php

--- view-footer.php
+++ view-footer.php
@@ -1,4 +1,4 @@
-    </div>
+</div class="container">
 <html>
 <body>
 
@@ -35,4 +35,4 @@ function myFunction() {
 </html>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </body>
-
+</div>
